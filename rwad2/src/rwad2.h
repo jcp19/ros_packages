a026c647beba30e7690c5d1e3b3394d030ccab77
João Pereira <joaopereira_19@hotmail.com>
2017-07-07 14:03:28 +0100

    added functionality to rwad

--- rwad2/src/rwad2.h
+++ rwad2/src/rwad2.h
@@ -1,8 +1,6 @@
 #ifndef RWAD2_H
 #define RWAD2_H
 
-#include "kobuki_msgs/SensorState.h"
-#include "std_msgs/UInt8.h"
 // Robot states
 enum STATE {DOCKED, WALKING, SEARCHING_DOCK};
 
@@ -15,10 +13,12 @@ const float BATTERY_CAPACITY = 160.0;
 #ifdef SIMULATION
   // when in simultaion, its assumed that the value of the battery
   // is published in the /bokuki_simulation/battery topic
+  #include "std_msgs/UInt8.h"
   #define BATTERY_TOPIC "/kobuki_simulation/battery"
   #define CALLBACK_ARG_TYPE std_msgs::UInt8
   #define BATTERY_FIELD data
 #else
+  #include "kobuki_msgs/SensorState.h"
   #define BATTERY_TOPIC "/mobile_base/sensors/core"
   #define CALLBACK_ARG_TYPE kobuki_msgs::SensorState
   #define BATTERY_FIELD battery
--- rwad2/src/rwad2_map.cpp
+++ rwad2/src/rwad2_map.cpp
@@ -1,32 +1,34 @@
 #include "ros/ros.h"
 #include "rwad2.h"
-
-// possibly not needed
-#include <actionlib/client/simple_action_client.h>
-#include <actionlib/client/terminal_state.h>
-#include <kobuki_msgs/AutoDockingAction.h>
-#include <move_base_msgs/MoveBaseAction.h>
-#include "kobuki_msgs/SensorState.h"
-#include "kobuki_msgs/Led.h"
 #include "std_msgs/Empty.h"
 #include "geometry_msgs/Twist.h"
-#include <sstream>
-#include <kobuki_msgs/AutoDockingActionGoal.h>
+#include "kobuki_msgs/Led.h"
 
 // Dock Coordinates
-float dockX;
-float dockY;
+float dockX = 1.41;
+float dockY = -0.418;
 
 // Coordinates 1 meter in front of dock
-float frontX;
-float frontY;
+float frontX = 0.81;
+float frontY = -0.124;
+float orientation_to_dock = 0.0;
 
 // Robot's state
 float robot_battery = 0.0;
 
-void doIt(ros::Publisher p){
+void do_it(ros::Publisher p){
     std_msgs::Empty e;
     p.publish(e);
+}
+
+void go_to_front_of_dock(int orientation = orientation_to_dock){
+
+    // Not Implemented
+}
+
+void dock(){
+    go_to_front_of_dock();
+    // Not Implemented
 }
 
 void battery_callback(const CALLBACK_ARG_TYPE::ConstPtr& msg){
@@ -38,10 +40,6 @@ void docked_state(ros::Rate rate){
     while(robot_battery <= UPPER_LIMIT){
         rate.sleep();
     }
-}
-
-void undocking(){
-
 }
 
 void walking_state(ros::Rate rate){
@@ -62,16 +60,16 @@ int main(int argc, char** argv){
     // subscribes to read battery levels
     ros::Subscriber sub = n.subscribe(BATTERY_TOPIC, 1, battery_callback);
 
-    ros::Publisher pubVel =  n.advertise<geometry_msgs::Twist>("input/rwad", 10);
-    ros::Publisher pubLed =  n.advertise<kobuki_msgs::Led>("mobile_base/commands/led2", 1);
+    ros::Publisher pubVel = n.advertise<geometry_msgs::Twist>("input/rwad", 10);
+    ros::Publisher pubLed = n.advertise<kobuki_msgs::Led>("mobile_base/commands/led2", 1);
 
     // controls the random walk activity
-    ros::Publisher enableRandWalk = n.advertise<std_msgs::Empty>("kobuki_random_walker/enable", 1);
-    ros::Publisher disableRandWalk = n.advertise<std_msgs::Empty>("kobuki_random_walker/disable", 1);
+    auto enableRandWalk = n.advertise<std_msgs::Empty>("kobuki_random_walker/enable", 1);
+    auto disableRandWalk = n.advertise<std_msgs::Empty>("kobuki_random_walker/disable", 1);
 
     // controls the safety controller activity
-    ros::Publisher enableSafetyController = n.advertise<std_msgs::Empty>("kobuki_safety_controller/enable", 1);
-    ros::Publisher disableSafetyController = n.advertise<std_msgs::Empty>("kobuki_safety_controller/disable", 1);
+    auto enableSafetyController = n.advertise<std_msgs::Empty>("kobuki_safety_controller/enable", 1);
+    auto disableSafetyController = n.advertise<std_msgs::Empty>("kobuki_safety_controller/disable", 1);
 
     // Checks if the battery levels are defined in the param server and loads
     // them if they do
@@ -91,22 +89,25 @@ int main(int argc, char** argv){
     // This loop goes through all the states of the automaton in each iteration
     while(ros::ok()){
         // Transition to docked state
-        doIt(disableRandWalk);
-        doIt(disableSafetyController);
+        do_it(disableRandWalk);
+        do_it(disableSafetyController);
 
         // Reached docked state
         docked_state(loop_rate);
 
-        // Transition to walking state
-        undocking();
-        doIt(enableRandWalk);
-        doIt(enableSafetyController);
+        // Transition to walking state a.k.a undocking
+        go_to_front_of_dock();
+        do_it(enableRandWalk);
+        do_it(enableSafetyController);
 
         // Reached docked state
         walking_state(loop_rate);
 
         // Reached dock searching state
         searching_dock_state(loop_rate);
+
+        // Move the robot to the dock
+        go_to_front_of_dock()
     }
 
     return 0;
